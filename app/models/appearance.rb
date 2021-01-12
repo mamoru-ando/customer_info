8d9e62bb1139d4c73a676bfb71916aac935ebb33
mamoru-ando <bsirregulars@yahoo.co.jp>
2021-01-12 21:38:00 +0900

    アソシエーションとスペル修正

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -1,2 +1,3 @@
 class Apprearance < ApplicationRecord
+  belongs_to :customer
 end
--- db/migrate/20210112120237_create_appearances.rb
+++ db/migrate/20210112120237_create_appearances.rb
@@ -1,6 +1,6 @@
-class CreateApprearances < ActiveRecord::Migration[6.0]
+class CreateAppearances < ActiveRecord::Migration[6.0]
   def change
-    create_table :apprearances do |t|
+    create_table :appearances do |t|
       t.text :text
       t.references :customer, foreign_key: true
 
