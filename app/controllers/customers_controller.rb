6f2b9860abdc2f4ca9b10aebda590f312b1eeb7c
mamoru-ando <bsirregulars@yahoo.co.jp>
2021-01-13 14:22:07 +0900

    顧客情報登録フォーム

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,4 +2,9 @@ class CustomersController < ApplicationController
 
   def index
   end
+
+  def new
+    @customer = Customer.new
+  end
+
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,7 @@
 class Customer < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :sex
+
   has_many :orders
   has_one :appearances
 end
