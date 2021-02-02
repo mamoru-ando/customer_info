621ba6eba989af4a966ef97b1c071ebab9b767a4
mamoru-ando <bsirregulars@yahoo.co.jp>
2021-02-02 12:33:11 +0900

    N+1問題のGem導入と解消

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,7 +3,7 @@ class CustomersController < ApplicationController
   before_action :set_customer, only: [:show, :edit, :update, :destroy]
 
   def index
-    @customers = Customer.includes(:user).where(user_id: current_user.id).order("updated_at DESC")
+    @customers = Customer.eager_load(:user, :orders).where(user_id: current_user.id).order(updated_at: "DESC")
   end
 
   def new
