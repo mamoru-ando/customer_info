0549e08c9d745803dffd4d9524f2fa2c5643035d
mamoru-ando <bsirregulars@yahoo.co.jp>
2021-02-04 11:43:38 +0900

    カタカナ入力の制限

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,7 +6,6 @@ class Customer < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true, length: { maximum: 20, message: 'は20文字以内で入力してください' }
-  validates :name_kana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。' }
   with_options format: { with: /\A\d{10,11}\z/, allow_blank: true, message: 'はハイフンなしで入力してください' } do
     validates :tell1
     validates :tell2
