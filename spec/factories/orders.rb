482b09773f37d57b63599782f86832405f9176cd
mamoru-ando <bsirregulars@yahoo.co.jp>
2021-02-06 17:57:30 +0900

    オーダー登録と編集の結合テストコード

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,8 +1,12 @@
 FactoryBot.define do
   factory :order do
-    date              { Faker::Date.between(from: 2.year.ago, to: Date.today) }
+    date              { Faker::Date.between(from: 1.year.ago, to: Date.today) }
     people            { Faker::Number.within(range: 1..10) }
-    pay               { 6000 }
+    table             { 'A' + "#{Faker::Number.within(range: 1..10)}" }
+    drink             { Faker::Beer.name }
+    food              { Faker::Food.dish }
+    pay               { Faker::Number.within(range: 1000..60000) }
+    order_memo        { Faker::Lorem.characters(number: 10) }
 
   association :customer
   end
---
+++ spec/system/orders_spec.rb
@@ -0,0 +1,107 @@
+require 'rails_helper'
+
+RSpec.describe 'オーダー入力', type: :system do
+  before do
+    @customer1 = FactoryBot.create(:customer)
+    @order = FactoryBot.build(:order)
+  end
+
+  it 'ログインしたユーザーはお客様詳細ページでオーダー情報を入力できる' do
+    # ログインする
+    visit new_user_session_path
+    fill_in 'メールアドレス', with: @customer1.user.email
+    fill_in 'パスワード', with: @customer1.user.password
+    find('input[name="commit"]').click
+    expect(current_path).to eq(root_path)
+    # お客様詳細ページへ遷移する
+    visit customer_path(@customer1)
+    # オーダー情報入力ボタンがあることを確認する
+    expect(page).to have_link 'オーダー情報入力', href: new_customer_order_path(@customer1)
+    # オーダー入力ページへ遷移する
+    visit new_customer_order_path(@customer1)
+    # フォームに情報を入力する
+    fill_in '来店日', with: @order.date
+    fill_in '来店人数', with: @order.people
+    fill_in 'テーブル番号', with: @order.table
+    fill_in 'ドリンクオーダー', with: @order.drink
+    fill_in 'フードオーダー', with: @order.food
+    fill_in '支払い金額', with: @order.pay
+    fill_in 'メモ', with: @order.order_memo
+    # 登録するとOrderモデルのカウントが1上がることを確認する
+    expect{
+      find('input[name="commit"]').click
+    }.to change { Order.count }.by(1)
+    # 詳細ページにリダイレクトされることを確認する
+    expect(current_path).to eq(customer_path(@customer1))
+    # 詳細ページに先程のオーダー情報が含まれていることを確認する
+    expect(page).to have_content(@order.date)
+  end
+
+  it 'ログインしていないとオーダー情報を登録できない' do
+    # ログインページへ移動する
+    visit new_user_session_path
+    # ログインページにはオーダー情報入力ボタンがないことを確認する
+    expect(page).to have_no_link 'オーダー情報入力', href: new_customer_order_path(@customer1)
+  end
+end
+
+
+RSpec.describe 'オーダー編集', type: :system do
+  before do
+    @order = FactoryBot.create(:order)
+  end
+
+  it 'ログインしていればオーダー情報を編集できる' do
+    # ログインする
+    visit new_user_session_path
+    fill_in 'メールアドレス', with: @order.customer.user.email
+    fill_in 'パスワード', with: @order.customer.user.password
+    find('input[name="commit"]').click
+    expect(current_path).to eq(root_path)
+    # お客様詳細ページへ遷移する
+    visit customer_path(@order.customer_id)
+    # オーダー編集ボタンがあることを確認する
+    expect(page).to have_link '編集', href: edit_customer_order_path(@order.customer_id, @order.id)
+    # オーダー編集ページへ移動する
+    visit edit_customer_order_path(@order.customer_id, @order.id)
+    # 登録したオーダー情報が入力されていることを確認する
+    expect(
+      find('#order_date').value
+    ).to eq("#{@order.date}")
+    expect(
+      find('#order_people').value
+    ).to eq("#{@order.people}")
+    expect(
+      find('#order_table').value
+    ).to eq(@order.table)
+    expect(
+      find('#order_drink').value
+    ).to eq(@order.drink)
+    expect(
+      find('#order_food').value
+    ).to eq(@order.food)
+    expect(
+      find('#order_pay').value
+    ).to eq("#{@order.pay}")
+    expect(
+      find('#order_order_memo').value
+    ).to eq(@order.order_memo)
+    # オーダーを編集する
+    fill_in 'order_order_memo', with: "#{@order.order_memo}+編集したテキスト"
+    # 登録してもOrderモデルのカウントは変わらないことを確認する
+    expect{
+      find('input[name="commit"]').click
+    }.to change { Order.count }.by(0)
+    # お客様詳細ページへ遷移したことを確認する
+    expect(current_path).to eq(customer_path(@order.customer))
+    # 編集した内容が表示されていることを確認する
+    expect(page).to have_content("#{@order.order_memo}+編集したテキスト")
+  end
+
+  it 'ログインしていないとオーダー情報は編集できない' do
+    # ログインページへ移動する
+    visit new_user_session_path
+    # ログインページにはオーダー編集ボタンはないことを確認する
+    expect(page).to have_no_link '編集', href: edit_customer_order_path(@order.customer_id, @order.id)
+  end
+end
